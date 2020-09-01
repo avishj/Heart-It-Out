3d68082ad52b3fda2a8835d6f7af721ae15ae637
avishj <avish.j@protonmail.com>
2020-09-01 14:47:40 +0530

    Added Social Media Routes + Removed Boilerplate

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,32 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/social/instagram',
+    beforeEnter() { location.href = 'https://www.instagram.com/sos_vit/' }
+  },
+  {
+    path: '/social/facebook',
+    beforeEnter() { location.href = 'https://www.facebook.com/Smile-Over-Stress-VIT-117344306734480/' }
+  },
+  {
+    path: '/social/linkedin',
+    beforeEnter() { location.href = 'https://in.linkedin.com/company/smile-over-stress-sos-vit' }
+  },
 ]
 
 const router = new VueRouter({
