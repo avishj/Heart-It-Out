06ad1315e5bf448a4a85db8587ad56bcfbbc1fab
avishj <avish.j@protonmail.com>
2020-09-01 18:45:01 +0530

    Completed Home Card Image Responsively

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <b-container fluid class="app-home">
     <Navbar />
+    <b-img
+      class="px--3 pt--2 image--main d-block mx-auto"
+      fluid
+      :src="require('../assets/img/HomeCard.png')"
+    ></b-img>
   </b-container>
 </template>
 
@@ -18,5 +23,15 @@ export default {
 .container-fluid {
   padding: 0 !important;
   margin: 0 !important;
+}
+.px--3 {
+  padding-left: 3vw;
+  padding-right: 3vw;
+}
+.pt--2 {
+  padding-top: 2vw;
+}
+.image--main {
+  min-width: 94vw;
 }
 </style>
