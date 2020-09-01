164ae5a6af00abc3f0f0a1861970496d7fcd6beb
avishj <avish.j@protonmail.com>
2020-09-01 14:59:42 +0530

    Added Template for social media

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,9 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item to="/social/instagram">Insta</b-nav-item>
+          <b-nav-item to="/social/facebook">FB</b-nav-item>
+          <b-nav-item to="/social/linkedin">LinkedIn</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
