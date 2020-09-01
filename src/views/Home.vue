7699dcd0f9e80483507692784f6701c224fd48b5
avishj <avish.j@protonmail.com>
2020-09-01 19:27:14 +0530

    Fixed Overflow Bug + Added the 2 buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,21 @@
       fluid
       :src="require('../assets/img/HomeCard.png')"
     ></b-img>
-    <div class="text-center text-color-b text-c1">4 SEPTEMBER - 6 SEPTEMBER</div>
+    <div class="text-center text-color-b text-c1 my--2">4 SEPTEMBER - 6 SEPTEMBER</div>
+    <b-row no-gutters class="my--2">
+      <b-col class="col-custom mx-auto" cols="6">
+        <b-button
+          class="text-c2 btn-custom"
+          block
+          size="lg"
+          variant="outline-color-c"
+          v-scroll-to="{el: '#rules', easing: 'ease'}"
+        >RULES</b-button>
+      </b-col>
+      <b-col class="col-custom mx-auto" cols="6">
+        <b-button block class="text-c2 btn-custom" size="lg" variant="color-c">REGISTER NOW</b-button>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -29,14 +43,40 @@ export default {
   padding-left: 3vw;
   padding-right: 3vw;
 }
+/* .mx--3 {
+  margin-left: 3vw;
+  margin-right: 3vw;
+} */
 .pt--2 {
   padding-top: 2vw;
 }
 .image--main {
   min-width: 94vw;
 }
+.p--1 {
+  padding-top: 1vh;
+  padding-bottom: 1vh;
+  padding-left: 1vw;
+  padding-right: 1vw;
+}
+.my--2 {
+  margin-top: 2vw;
+  margin-bottom: 2vw;
+}
 .text-c1 {
   font-family: "Bison Bold";
   font-size: 3rem;
+}
+.text-c2 {
+  font-family: "Bison Bold";
+  font-size: 2.5rem;
+}
+.col-custom {
+  max-width: 30%;
+  display: block;
+}
+.btn-custom {
+  border-radius: 1rem;
+  border-width: 0.35rem;
 }
 </style>
