bf415b4d5c86909396eaba95d397cf7cdd1311b5
PoornaChandra <developer.poornachandra@gmail.com>
2020-09-02 23:00:03 +0530

    add mobile style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,9 @@
       fluid
       :src="require('../assets/img/HomeCard.png')"
     ></b-img>
-    <div class="text-center text-color-b text-c1 my--2">4 SEPTEMBER - 6 SEPTEMBER</div>
+    <div class="text-center text-color-b text-c1 my--2">
+      4 SEPTEMBER - 6 SEPTEMBER
+    </div>
     <br />
     <b-row no-gutters class="px--3 my--2" cols="1" cols-md="2">
       <b-col class="col-custom mr-auto">
@@ -15,11 +17,17 @@
           block
           size="lg"
           variant="outline-color-c"
-          v-scroll-to="{el: '#rules', easing: 'ease'}"
-        >RULES</b-button>
+          v-scroll-to="{ el: '#rules', easing: 'ease' }"
+          >RULES</b-button
+        >
       </b-col>
       <b-col class="col-custom ml-auto">
-        <b-button block class="text-c2 btn-custom" size="lg" variant="color-c">REGISTER</b-button>
+        <b-button block class="text-c2 btn-custom" size="lg" variant="color-c"
+          ><a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSerEAeKSC-UzAIghP4kOISXrJP2_1hvPuHgw6B1Waw5uubz2g/viewform"
+            >REGISTER</a
+          ></b-button
+        >
       </b-col>
     </b-row>
     <br />
@@ -29,12 +37,15 @@
       <br />
       <div class="text-c4">
         A webinar will be held about the art competition.
-        <br />A speaker session will be held on the importance of art in life and its valuable role in mental health.
+        <br />A speaker session will be held on the importance of art in life
+        and its valuable role in mental health.
       </div>
       <br />
       <div class="text-c3">September 5 & 6</div>
       <br />
-      <div class="text-c4">The entries to the competition shall be accepted via a form.</div>
+      <div class="text-c4">
+        The entries to the competition shall be accepted via a form.
+      </div>
     </div>
     <br />
     <br />
@@ -47,20 +58,29 @@
       <div class="text-c6">
         1) Content must be original.
         <br />2) Submit within the deadline (11:59 PM, Sunday, September 6).
-        <br />3) Submit only 1 artwork per person.
-        <br />4) Artwork must follow the theme of emotion. (Pick any emotion to convey)
-        <br />5) While submitting, specify the emotion that you've chosen, and the idea behind your work.
-        <br />6) Upload 2 shots, one progress shot (50% completed), and one of your completed work.
+        <br />3) Submit only 1 artwork per person. <br />4) Artwork must follow
+        the theme of emotion. (Pick any emotion to convey) <br />5) While
+        submitting, specify the emotion that you've chosen, and the idea behind
+        your work. <br />6) Upload 2 shots, one progress shot (50% completed),
+        and one of your completed work.
       </div>
       <br />
     </div>
     <div class="text-center text-color-b text-c1 my--2">EVENT BY</div>
     <b-row class="row-custom">
       <b-col class="my--2" md="6">
-        <b-img class="d-block mx-auto" fluid :src="require('../assets/img/SOSLogoFooter.png')"></b-img>
+        <b-img
+          class="d-block mx-auto logo1"
+          fluid
+          :src="require('../assets/img/SOSLogoFooter.png')"
+        ></b-img>
       </b-col>
       <b-col class="my--2" md="6">
-        <b-img class="d-block mx-auto" fluid :src="require('../assets/img/TFACLogoFooter.png')"></b-img>
+        <b-img
+          class="d-block mx-auto logo2"
+          fluid
+          :src="require('../assets/img/TFACLogoFooter.png')"
+        ></b-img>
       </b-col>
     </b-row>
   </b-container>
@@ -71,8 +91,8 @@ import Navbar from "../components/Navbar";
 export default {
   name: "Home",
   components: {
-    Navbar
-  }
+    Navbar,
+  },
 };
 </script>
 
@@ -136,7 +156,7 @@ export default {
   font-family: "Bison Bold";
 }
 .text-c6 {
-  font-size: 2rem;
+  font-size: 1.5rem;
   color: white;
   font-family: "SF Pro Display Semibold";
 }
@@ -151,5 +171,58 @@ export default {
 .page-fill {
   height: 100vh;
   width: 100%;
+}
+
+.logo2 {
+  width: 150px;
+  height: 150px;
+}
+
+.logo1 {
+  width: 350px;
+  height: 150px;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:hover {
+  text-decoration: none;
+  color: inherit;
+}
+@media (max-width: 576px) {
+  .text-c1 {
+    font-family: "Bison Bold";
+    font-size: 3rem;
+  }
+  .text-c3 {
+    font-size: 1.5rem;
+    font-weight: bold;
+    font-family: "SF Pro Display Semibold";
+  }
+  .text-c4 {
+    font-size: 1rem;
+    font-family: "SF Pro Display Regular";
+  }
+  .text-c5 {
+    font-size: 3rem;
+    color: white;
+    font-weight: bolder;
+    font-family: "Bison Bold";
+  }
+  .text-c6 {
+    font-size: 1rem;
+    color: white;
+    font-family: "SF Pro Display Semibold";
+  }
+  .logo1 {
+    width: 250px;
+    height: 100px;
+  }
+  .logo2 {
+    width: 100px;
+    height: 100px;
+  }
 }
 </style>
