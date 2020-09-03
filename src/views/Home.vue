38ddb8bab906a1ff391ede9fc3c5f86525ddc337
PoornaChandra <developer.poornachandra@gmail.com>
2020-09-03 12:13:29 +0530

    change rules, logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,15 +36,16 @@
       <div class="text-c3">September 4 (6:00 - 8:00 PM)</div>
       <br />
       <div class="text-c4">
-        A webinar will be held about the art competition.
-        <br />A speaker session will be held on the importance of art in life
-        and its valuable role in mental health.
+        Do you know about importance of art in healing people?
+        <br />Art has the power to heal your inner wounds and bring out your
+        true emotions. Participate in this webinar to know more.
       </div>
       <br />
       <div class="text-c3">September 5 & 6</div>
       <br />
       <div class="text-c4">
-        The entries to the competition shall be accepted via a form.
+        The webinar is followed by an Art Competition. Exciting cash prizes and
+        certificates to be won. So, come and express your emotions!
       </div>
     </div>
     <br />
@@ -59,25 +60,35 @@
         1) Content must be original.
         <br />2) Submit within the deadline (11:59 PM, Sunday, September 6).
         <br />3) Submit only 1 artwork per person. <br />4) Artwork must follow
-        the theme of emotion. (Pick any emotion to convey) <br />5) While
-        submitting, specify the emotion that you've chosen, and the idea behind
-        your work. <br />6) Upload 2 shots, one progress shot (50% completed),
-        and one of your completed work.
+        the theme of emotion. (Pick any emotion to convey) <br />5)Theme is to
+        Express your emotions and working together to prevent suicide, through
+        art eg. Happiness, confusion, frustration, etc<br />6)Digital Art can
+        not be considered.<br />
+        7) While submitting, specify the emotion that you've chosen, and the
+        idea behind your work. <br />8) Upload 2 shots, one progress shot (50%
+        completed), and one of your completed work.
       </div>
       <br />
     </div>
-    <div class="text-center text-color-b text-c1 my--2">EVENT BY</div>
+    <div class="text-center text-color-b text-c1">EVENT BY</div>
     <b-row class="row-custom">
-      <b-col class="my--2" md="6">
+      <b-col class="my--2 logo1" md="4" sm="4">
         <b-img
-          class="d-block mx-auto logo1"
+          class="d-block mx-auto"
           fluid
-          :src="require('../assets/img/SOSLogoFooter.png')"
+          :src="require('../assets/img/CousellingDivisionRoundFooter.png')"
         ></b-img>
       </b-col>
-      <b-col class="my--2" md="6">
+      <b-col class="my--2 logo2" md="4" sm="4">
         <b-img
-          class="d-block mx-auto logo2"
+          class="d-block mx-auto"
+          fluid
+          :src="require('../assets/img/SOSRoundFooter.png')"
+        ></b-img>
+      </b-col>
+      <b-col class="my--2 logo3" md="4" sm="8">
+        <b-img
+          class="d-block mx-auto"
           fluid
           :src="require('../assets/img/TFACLogoFooter.png')"
         ></b-img>
@@ -173,16 +184,6 @@ export default {
   width: 100%;
 }
 
-.logo2 {
-  width: 150px;
-  height: 150px;
-}
-
-.logo1 {
-  width: 350px;
-  height: 150px;
-}
-
 a {
   text-decoration: none;
   color: inherit;
@@ -191,7 +192,13 @@ a:hover {
   text-decoration: none;
   color: inherit;
 }
-@media (max-width: 576px) {
+
+.img-fluid {
+  max-width: 50%;
+  height: auto;
+}
+
+@media (max-width: 768px) {
   .text-c1 {
     font-family: "Bison Bold";
     font-size: 3rem;
@@ -216,13 +223,67 @@ a:hover {
     color: white;
     font-family: "SF Pro Display Semibold";
   }
+
   .logo1 {
-    width: 250px;
-    height: 100px;
+    width: 150px;
+    height: 150px;
   }
+
   .logo2 {
-    width: 100px;
-    height: 100px;
+    width: 140px;
+    height: 140px;
+  }
+
+  .logo3 {
+    width: 140px;
+    height: 140px;
+  }
+
+  .my--2 + .logo3 {
+    margin-left: 30vw;
+  }
+
+  .my--2 {
+    margin-top: 2vw;
+    margin-bottom: 2vw;
+    margin-left: 12vw;
+  }
+
+  .img-fluid {
+    max-width: 100%;
+    height: auto;
+  }
+}
+
+@media (max-width: 400px) {
+  .logo1 {
+    width: 120px;
+    height: 120px;
+  }
+
+  .logo2 {
+    width: 110px;
+    height: 110px;
+  }
+
+  .logo3 {
+    width: 110px;
+    height: 110px;
+  }
+
+  .my--2 + .logo3 {
+    margin-left: 30vw;
+  }
+
+  .my--2 {
+    margin-top: 2vw;
+    margin-bottom: 2vw;
+    margin-left: 12vw;
+  }
+
+  .img-fluid {
+    max-width: 100%;
+    height: auto;
   }
 }
 </style>
